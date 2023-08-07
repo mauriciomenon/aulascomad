b99d38b5bd7a5b92d266c0691b8aed227c116007
menon <MENON@ITAIPU.GOV.BR>
2023-08-07 15:48:57 -0300

    implementacao de filtragem com regex

--- Outros/pdf2text/getEmails.py
+++ Outros/pdf2text/getEmails.py
@@ -1,39 +1,45 @@
 import PyPDF2
-import csv
+import re
+
 
 def extract_emails_from_pdf(pdf_filename):
     emails = []
-    
+
     with open(pdf_filename, 'rb') as file:
-        reader = PyPDF2.PdfFileReader(file)
-        
-        # Para cada página no PDF
-        for page_num in range(reader.numPages):
-            text = reader.getPage(page_num).extractText()
-            # Divide o texto em linhas
+        reader = PyPDF2.PdfReader(file)
+
+        for page_num in range(len(reader.pages)):
+            text = reader.pages[page_num].extract_text()
             lines = text.split("\n")
-            
+
             for line in lines:
-                # Procura linhas que parecem ser e-mails
-                if "@" in line:
-                    emails.append(line.strip())
-    
+                # Padrão para detectar um e-mail e removê-lo junto com qualquer texto após .com ou .com.br
+                match = re.search(r"([\w._%+-]+@[\w.-]+\.(com|com\.br))", line)
+                if match:
+                    emails.append(match.group(1))
+
     return emails
 
-def save_emails_to_csv(emails, csv_filename):
-    with open(csv_filename, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(["Email"]) # Cabeçalho da coluna
-        
+
+def save_emails_to_txt(emails, txt_filename):
+    with open(txt_filename, 'w') as txtfile:
         for email in emails:
-            writer.writerow([email])
+            txtfile.write(email + '\n')
+
 
 def main():
-    pdf_filename = 'path_to_your_pdf_file.pdf'
-    csv_filename = 'output_emails.csv'
-    
+    pdf_filename = input(
+        "Por favor, insira o caminho completo para o arquivo PDF: ")
+    txt_filename = pdf_filename.rsplit('.', 1)[0] + '.txt'
+
+    print(
+        f"Um arquivo de saída com o nome '{txt_filename}' será criado no mesmo diretório.")
+
     emails = extract_emails_from_pdf(pdf_filename)
-    save_emails_to_csv(emails, csv_filename)
+    save_emails_to_txt(emails, txt_filename)
+
+    print(f"Lista de emails salva em '{txt_filename}'.")
+
 
 if __name__ == '__main__':
     main()
