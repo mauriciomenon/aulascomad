ba684586b3294688946b158b5cc52acbd9c6ec02
menon <MENON@ITAIPU.GOV.BR>
2023-08-08 09:28:23 -0300

    Considerar possiveis emails com chars em maisculo alem de organizar em ordem alfabetica

--- Outros/pdf2text/getEmails.py
+++ Outros/pdf2text/getEmails.py
@@ -14,38 +14,42 @@ def extract_emails_from_pdf(pdf_filename):
             lines = text.split("\n")
 
             for line in lines:
-                match = re.search(r"([\w._%+-]+@[\w.-]+\.(com|com\.br))", line)
+                match = re.search(
+                    r"([\w._%+-]+@[\w.-]+\.(com|com\.br))", line, re.IGNORECASE)
                 if match:
-                    emails.add(match.group(1))  # Adiciona o e-mail ao conjunto
+                    # Adiciona o e-mail ao conjunto
+                    emails.add(match.group(1).lower()) # converte para minusculo os emails (TOC)
 
     # Converte de volta para lista
     return list(emails)
 
 
 def save_emails_to_txt(emails, txt_filename):
+    sorted_emails = sorted(emails) #ordem alfabetica
     with open(txt_filename, 'w') as txtfile:
-        for email in emails:
+        for email in sorted_emails:
             txtfile.write(email + '\n')
 
-
 def main():
-    if len(sys.argv) > 1:
-        # caminho do arquivo  na linha de comando
-        pdf_filename = sys.argv[1]
-    else:
-        pdf_filename = input(
-            "Insira o caminho completo para o arquivo PDF: ")
+    try:
+        if len(sys.argv) > 1:
+            # caminho do arquivo  na linha de comando
+            pdf_filename = sys.argv[1]
+        else:
+            pdf_filename = input(
+                "Insira o caminho completo para o arquivo PDF: ")
 
-    txt_filename = pdf_filename.rsplit('.', 1)[0] + '.txt'
+        txt_filename = pdf_filename.rsplit('.', 1)[0] + '.txt'
 
-    print(
-        f"O arquivo '{txt_filename}' sera criado no mesmo diretorio.")
+        print(
+            f"O arquivo '{txt_filename}' sera criado no mesmo diretorio.")
 
-    emails = extract_emails_from_pdf(pdf_filename)
-    save_emails_to_txt(emails, txt_filename)
+        emails = extract_emails_from_pdf(pdf_filename)
+        save_emails_to_txt(emails, txt_filename)
 
-    print(f"Lista de emails salva em '{txt_filename}'.")
-
+        print(f"Lista de emails salva em '{txt_filename}'.")
+    except KeyboardInterrupt:
+        print("Programa interrompido pelo usuário.")    
 
 if __name__ == '__main__':
     main()
