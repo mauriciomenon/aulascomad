870dc99a0f5f4d109207bfbc927c4bcf9a7c25b0
menon <MENON@ITAIPU.GOV.BR>
2024-07-16 12:07:04 -0300

    mp3

--- fading.py
+++ fading.py
@@ -1,12 +1,16 @@
 import pyttsx3
 import os
+from pydub import AudioSegment
 
-def text_to_speech(text, output_file):
+def text_to_speech(text, output_wav, output_mp3):
     # Inicialize o motor TTS
     engine = pyttsx3.init()
     
+        # Obtenha todas as vozes disponíveis
+    voices = engine.getProperty('voices')
+    
     # Defina a taxa de fala (velocidade)
-    engine.setProperty('rate', 150)  # Ajuste para uma taxa moderada
+    engine.setProperty('rate', 200)  # Ajuste para uma taxa moderada
     
     # Defina o volume
     engine.setProperty('volume', 1.0)  # Defina o volume máximo
@@ -24,27 +28,51 @@ def text_to_speech(text, output_file):
         engine.setProperty('voice', voices[0].id)
     
     # Converta texto em fala e salve em arquivo WAV
-    engine.save_to_file(text, output_file)
+    engine.save_to_file(text, output_wav)
     
     # Também toque o texto
     engine.say(text)
     
     # Execute o processo de conversão e reprodução
     engine.runAndWait()
-
-if __name__ == "__main__":
-    # Texto a ser convertido em fala
-    text = ("Bom dia brabona, esse é um exemplo de conversão de texto para áudio usando paiton. "
-            "Acabei de descobrir como funciona esse treco e achei muito legal. beijos para você")
     
-    # Nome do arquivo de saída
-    output_file = "recado.wav"
-    
-    # Chame a função para converter texto em fala, tocar e salvar em arquivo
-    text_to_speech(text, output_file)
+    # Converter WAV para MP3 usando pydub
+    sound = AudioSegment.from_wav(output_wav)
+    sound.export(output_mp3, format="mp3")
     
     # Confirme que o arquivo foi salvo
-    if os.path.exists(output_file):
-        print(f"O arquivo de áudio foi salvo como {output_file}")
+    if os.path.exists(output_mp3):
+        print(f"O arquivo de áudio foi salvo como {output_mp3}")
     else:
         print("Falha ao salvar o arquivo de áudio.")
+        
+def list_available_voices():
+    # Inicialize o motor TTS
+    engine = pyttsx3.init()
+    
+    # Obtenha todas as vozes disponíveis
+    voices = engine.getProperty('voices')
+    
+    # Listar todas as vozes com seus detalhes
+    for voice in voices:
+        print(f"ID: {voice.id}")
+        print(f"Name: {voice.name}")
+        print(f"Languages: {voice.languages}")
+        print(f"Gender: {voice.gender}")
+        print(f"Age: {voice.age}")
+        print()
+
+if __name__ == "__main__":
+    
+    list_available_voices()
+    
+    # Texto a ser convertido em fala
+    text = ("Que tal uma empanada. "
+            "Uma especial, de alta raridade, de 58 Hertz. ")
+    
+    # Nomes dos arquivos de saída
+    output_wav = "recado.wav"
+    output_mp3 = "recado.mp3"
+    
+    # Chame a função para converter texto em fala, tocar e salvar em arquivos WAV e MP3
+    text_to_speech(text, output_wav, output_mp3)
