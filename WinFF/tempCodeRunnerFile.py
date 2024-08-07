e94bb74604ad0a775db2409af6f3feaf3c60690b
menon <MENON@ITAIPU.GOV.BR>
2024-08-07 12:42:55 -0300

    disponibilização de versao exe

--- WinFF/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-# Caixa de texto para exibir o comando do FFmpeg
-tk.Label(root, text="Comando FFmpeg:").grid(row=13, column=0, padx=10, pady=5, sticky="w")
-command_display = tk.Text(root, height=3, width=80, font=("TkDefaultFont", 9))
-command_display.grid(row=14, column=0, columnspan=3, padx=10, pady=5)
