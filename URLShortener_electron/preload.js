fd39769d8c74637967723d3d20ce1e52b646e9fa
menon <mauriciomenon@users.noreply.github.com>
2025-01-16 11:02:15 -0300

    limpeza LFS

--- URLShortener_electron/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  shortenUrl: (url) => ipcRenderer.invoke('shorten-url', url),
-  copyToClipboard: (text) => ipcRenderer.invoke('copy-to-clipboard', text),
-});
